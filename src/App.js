46e715581f7a6ea4ba499cbaff62fbcd4036463d
Surukati Asutosh Dora <78288601+SurukatiAsutoshDora@users.noreply.github.com>
2022-08-04 19:58:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <p className="para" >Click on Request Button to Fetch the API on the console </p>
+    </div>
+  );
+}
+
+export default App;
